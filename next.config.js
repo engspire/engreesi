c982708cb9b6f788853a75b833ff271421ff18ea
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2023-11-05 18:41:22 +0530

    Update PWA config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,8 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV !== 'production'
 });
 
-module.exports = process.env.NODE_ENV === "production" ? withPWA(withNextra()) : withNextra();
+module.exports = withPWA(withNextra());
 
---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon512_maskable.png"></link>
+        <meta name="theme-color" content="#171717" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
